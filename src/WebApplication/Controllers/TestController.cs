aec779d8765634500693414b8dc54592a14055f4
w.bitar@idscanbiometrics.com <w.bitar@idscan.co.uk>
2016-05-26 22:18:37 +0300

    Add simple API and LoadTest Projects

---
+++ src/WebApplication/Controllers/TestController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace WebApplication.Controllers
+{
+    public class ApiModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+
+    public class TestController : ApiController
+    {
+        private static ConcurrentDictionary<Guid,ApiModel> list = new ConcurrentDictionary<Guid, ApiModel>();
+        private static Random rnd = new Random();
+        /*[HttpGet]
+        [Route("api/Test/{id}")]
+        public IHttpActionResult GetById(Guid id)
+        {
+            var model = list.FirstOrDefault(x => x.Id == id);
+            return Ok(model);
+        }*/
+
+        [HttpPost]
+        [Route("api/Test")]
+        public IHttpActionResult Add(ApiModel model)
+        {
+            var val = rnd.Next(2000000, 200000000);
+            for (int i = 0; i < val; i++)
+            {
+                Thread.Sleep(100);
+                var x = i / val * 213 ^ 12312;
+            }
+            if (!list.TryAdd(model.Id, model))
+            {
+                throw new Exception("Failed in Add");
+            }
+            return Ok(model);
+        }
+    }
+}
---
+++ src/WebLoadTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebLoadTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Random rnd = new Random();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var val = rnd.Next(2000000, 200000000);
+            for (int i = 0; i < val; i++)
+            {
+                Thread.Sleep(100);
+                var x = i/val*213 ^ 12312;
+            }
+        }
+    }
+}
