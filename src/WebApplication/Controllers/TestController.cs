c2fab573fd45f8d19c172b8844975e432c07cd71
WahidBitar <Wahid.wb@gmail.com>
2016-05-31 00:06:08 +0300

    Remove the test project Add good timed result

--- src/WebApplication/Controllers/TestController.cs
+++ src/WebApplication/Controllers/TestController.cs
@@ -13,73 +13,55 @@ namespace WebApplication.Controllers
     public class ApiModel
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Value { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        /*public int DataLoopCount { get; set; }
+        public string Data { get; set; }*/
+
+        public double ProcessTime
+        {
+            get { return (Finish - Start).TotalMilliseconds; }
+        }
     }
+
 
 
     public class TestController : ApiController
     {
-        private static ConcurrentDictionary<Guid, ApiModel> list = new ConcurrentDictionary<Guid, ApiModel>();
-        private static Random rnd = new Random();
-        private static bool sw = false;
-        private static object loc = new object();
+        private static ConcurrentDictionary<Guid, ApiModel> modelDictionary = new ConcurrentDictionary<Guid, ApiModel>();
+        private static Random random = new Random();
+        private static bool shouldBeLong = false;
+        private static object lok = new object();
 
-        /*[HttpGet]
-        [Route("api/Test/{id}")]
-        public IHttpActionResult GetById(Guid id)
-        {
-            var model = list.FirstOrDefault(x => x.Id == id);
-            return Ok(model);
-        }*/
-
-        [HttpPost]
-        [Route("api/Test")]
-        public IHttpActionResult Add(ApiModel model)
-        {
-            var val = rnd.Next(2000000, 200000000);
-            for (int i = 0; i < val; i++)
-            {
-                var x = i/val; //* 213 ^ 12312;
-            }
-            Thread.Sleep(rnd.Next(190, 950));
-            model.Id = Guid.NewGuid();
-            if (!list.TryAdd(model.Id, model))
-            {
-                throw new Exception("Failed in Add");
-            }
-            return Ok(model);
-        }
 
         [HttpGet]
         [Route("api/Get/Test")]
         public IHttpActionResult Get()
         {
-            /*lock (loc)
+            var startDate = DateTime.UtcNow;
+            var stringBuilder = new StringBuilder();
+            var loopCount = 0;
+            int intervalTime;
+            lock (lok)
             {
-                Thread.Sleep(sw ? 600 : 1200);
-                sw = !sw;
-            }*/
-            var model = new ApiModel();
-            var str = new StringBuilder();
-            var val = rnd.Next(50000, 500000);
-            for (int i = 0; i < val; i++)
-            {
-                var x = i/val*213 ^ 7;
-                str.AppendLine(@"a");
+                intervalTime = shouldBeLong ? 1200 : 200;
+                shouldBeLong = !shouldBeLong;
             }
-            model.Value = str.ToString();
-            var s = rnd.Next(600, 1800);
-            Thread.Sleep(s);
-
-            model.Id = Guid.NewGuid();
-            if (!list.TryAdd(model.Id, model))
+            while (startDate.AddMilliseconds(intervalTime) > DateTime.UtcNow)
             {
-                throw new Exception("Failed in Add");
+                /*loopCount = loopCount + 1;
+                stringBuilder.Append("a");*/
             }
-            if (s > 1500)
-                return BadRequest();
-            return Ok(model);
+            var result = new ApiModel
+            {
+                Id = Guid.NewGuid(),
+                Start = startDate,
+                Finish = DateTime.UtcNow,
+                /*Data = stringBuilder.ToString(),
+                DataLoopCount = loopCount,*/
+            };
+            return Ok(result);
+            //return Ok(string.Format("Start At : {0} - Done at {1}",startDate.ToString("O"), DateTime.UtcNow.ToString("O")));
         }
     }
 }
