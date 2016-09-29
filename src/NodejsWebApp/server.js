14aeab63d15e4980a7eadd1ac6047fa6a18aab3f
w.bitar@idscanbiometrics.com <w.bitar@idscan.co.uk>
2016-09-29 21:25:31 +0300

    Hello world with Express

--- src/NodejsWebApp/server.js
+++ src/NodejsWebApp/server.js
@@ -1,6 +1,13 @@
 ﻿var http = require('http');
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+console.log(port);
+
+var express = require('express');
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hello world");
+});
+
+var server = http.createServer(app);
+server.listen(port);
