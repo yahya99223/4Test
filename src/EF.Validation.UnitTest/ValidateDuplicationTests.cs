dc1d703e0db19af9aff4186da4ed043ae0c2a58a
w.bitar@idscanbiometrics.com <w.bitar@idscan.co.uk>
2016-11-07 16:31:04 +0300

    Added NUnit tests for adding

--- src/EF.Validation.UnitTest/ValidateDuplicationTests.cs
+++ src/EF.Validation.UnitTest/ValidateDuplicationTests.cs
@@ -2,41 +2,41 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Core;
+using System.Data.Entity.Validation;
 using EF.Validation.Model;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 
 
 namespace EF.Validation.UnitTest
 {
-    [TestClass]
+    [TestFixture]
     public class ValidateDuplicationTests
     {
-        private static MyDbContext dbContext;
-        private static DbContextTransaction dbContextTransaction;
-        private static readonly Guid defaultCompanyId = Guid.Parse("{BD61EA16-DD98-4196-B0D1-727C3BDA2DF9}");
-        private static readonly Guid firstUserId = Guid.Parse("{F18086F1-865C-424C-91ED-76D04AE26B74}");
-        private static readonly Guid secondUserId = Guid.Parse("{7D9398C3-E2BD-4722-A81A-712585DD2E5D}");
-        //private static Guid defaultCompanyId = Guid.Parse("{7BFE442F-9B51-4BCE-9096-BEE1056A179E}");
+        private MyDbContext dbContext;
+        private DbContextTransaction dbContextTransaction;
+        private readonly Guid defaultCompanyId = Guid.Parse("{BD61EA16-DD98-4196-B0D1-727C3BDA2DF9}");
+        private readonly Guid firstUserId = Guid.Parse("{F18086F1-865C-424C-91ED-76D04AE26B74}");
+        private readonly Guid secondUserId = Guid.Parse("{7D9398C3-E2BD-4722-A81A-712585DD2E5D}");
 
 
-        [ClassInitialize()]
-        public static void ClassInit(TestContext context)
+        [OneTimeSetUp]
+        public void ClassInit()
         {
             dbContext = new MyDbContext();
             dbContextTransaction = dbContext.Database.BeginTransaction();
         }
 
 
-        [ClassCleanup()]
-        public static void ClassCleanup()
+        [OneTimeTearDown]
+        public void ClassCleanup()
         {
-            dbContextTransaction.Commit();
+            dbContextTransaction.Rollback();
             dbContextTransaction.Dispose();
             dbContext.Dispose();
         }
 
 
-        [TestMethod]
+        [Test, Order(1)]
         public void Add_First_Person_Should_Success()
         {
             var person = new Person
@@ -53,7 +53,7 @@ namespace EF.Validation.UnitTest
         }
 
 
-        [TestMethod]
+        [Test, Order(2)]
         public void Add_Second_Person_Should_Success()
         {
             var person = new Person
@@ -70,39 +70,45 @@ namespace EF.Validation.UnitTest
         }
 
 
-        [TestMethod]
-        [ExpectedException(typeof(UpdateException))]
+        [Test, Order(3)]
         public void Add_Person_With_Duplicated_Name_Should_Throw_Exception()
         {
-            var person = new Person
-            {
-                Id = Guid.NewGuid(),
-                ManagerId = firstUserId,
-                CompanyId = defaultCompanyId,
-                Name = "user2",
-                Email = "user3@domain.com"
-            };
+            Assert.Throws<DbEntityValidationException>(() =>
+                                                       {
+                                                           var person = new Person
+                                                           {
+                                                               Id = Guid.NewGuid(),
+                                                               ManagerId = firstUserId,
+                                                               CompanyId = defaultCompanyId,
+                                                               Name = "user2",
+                                                               Email = "user3@domain.com"
+                                                           };
 
-            dbContext.People.Add(person);
-            dbContext.SaveChanges();
+                                                           dbContext.People.Add(person);
+                                                           dbContext.SaveChanges();
+                                                       }
+            );
         }
 
 
-        [TestMethod]
-        [ExpectedException(typeof(UpdateException))]
+        [Test, Order(4)]
         public void Add_Person_With_Duplicated_Email_Should_Throw_Exception()
         {
-            var person = new Person
-            {
-                Id = Guid.NewGuid(),
-                ManagerId = firstUserId,
-                CompanyId = defaultCompanyId,
-                Name = "user3",
-                Email = "user2@domain.com"
-            };
+            Assert.Throws<DbEntityValidationException>(() =>
+                                                       {
+                                                           var person = new Person
+                                                           {
+                                                               Id = Guid.NewGuid(),
+                                                               ManagerId = firstUserId,
+                                                               CompanyId = defaultCompanyId,
+                                                               Name = "user3",
+                                                               Email = "user2@domain.com"
+                                                           };
 
-            dbContext.People.Add(person);
-            dbContext.SaveChanges();
+                                                           dbContext.People.Add(person);
+                                                           dbContext.SaveChanges();
+                                                       }
+            );
         }
     }
 }
