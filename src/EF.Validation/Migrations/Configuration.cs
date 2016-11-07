cc90f70db66249d6dccfb66ef904e753ffa6f39c
w.bitar@idscanbiometrics.com <w.bitar@idscan.co.uk>
2016-11-07 16:01:51 +0300

    Added seed for company

--- src/EF.Validation/Migrations/Configuration.cs
+++ src/EF.Validation/Migrations/Configuration.cs
@@ -21,25 +21,15 @@ namespace EF.Validation.Migrations
 
         protected override void Seed(EF.Validation.MyDbContext context)
         {
-           /* var defaultCompanyId = Guid.Parse("{BD61EA16-DD98-4196-B0D1-727C3BDA2DF9}");
+            var defaultCompanyId = Guid.Parse("{BD61EA16-DD98-4196-B0D1-727C3BDA2DF9}");
             var company = new Company()
             {
                 Id = defaultCompanyId,
                 Name = "Fake Company",
-                People = new List<Person>
-                {
-                    new Person
-                    {
-                        Id = Guid.Parse("{4FA2D064-1F88-43CF-BA03-C344715A3315}"),
-                        CompanyId = defaultCompanyId,
-                        Name = "Ahmad",
-                        Email = "user1@domain.com"
-                    }
-                }
             };
 
             context.Companies.AddOrUpdate(company);
-            context.SaveChanges();*/
+            context.SaveChanges();
         }
     }
 }
