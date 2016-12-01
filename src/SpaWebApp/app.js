4cf922daedb402b3a3cd43dfbbbc503b14a5593b
w.bitar@idscanbiometrics.com <w.bitar@idscan.co.uk>
2016-12-01 09:35:08 +0300

    sorting using orderBy filter

--- src/SpaWebApp/app.js
+++ src/SpaWebApp/app.js
@@ -1,20 +1,32 @@
-﻿(function () {
+﻿(function() {
 
-    var app = angular.module('githubViewer', []);
+    var app = angular.module("githubViewer", []);
 
-    app.controller('MainController', ["$scope", "$http", function ($scope, $http) {
+    app.controller("MainController",
+    [
+        "$scope", "$http", function($scope, $http) {
+            $scope.welcomeMessage = "Github User Viewer";
+            $scope.userName = "angular";
+            $scope.repositorySortOrder = "-stargazers_count";
+            
+            var onError = function (reason) {
+                $scope.error = "Error in connection";
+            };
 
-        var onGetUserComplete = function (response) {
-            $scope.user = response.data;
+            var onGetRepositoriesComplete = function(response) {
+                $scope.repositories = response.data;
+            };
+
+            var onGetUserComplete = function(response) {
+                $scope.user = response.data;
+                $http.get($scope.user.repos_url)
+                    .then(onGetRepositoriesComplete, onError);
+            };
+
+            $scope.search = function(userName) {
+                $http.get("https://api.github.com/users/" + userName)
+                    .then(onGetUserComplete, onError);
+            };
         }
-
-
-        var onGetUserError = function (reason) {
-            $scope.error = "Error in connection";
-        }
-
-        $http.get("https://api.github.com/users/docker")
-            .then(onGetUserComplete, onGetUserError);
-
-    }]);
+    ]);
 }());
