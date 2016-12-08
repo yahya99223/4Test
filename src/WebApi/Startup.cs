bf8c6fd3dd545b555e1c8f8fcf6e98d79c6362db
w.bitar@idscanbiometrics.com <w.bitar@idscan.co.uk>
2016-12-08 12:36:53 +0300

    Created WebApi

---
+++ src/WebApi/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Owin;
+
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var configuration = new HttpConfiguration();
+            configuration.Routes.MapHttpRoute(
+                             "default",
+                             "api/{controller}"
+                         );
+
+            app.UseWebApi(configuration);
+        }
+    }
+}
