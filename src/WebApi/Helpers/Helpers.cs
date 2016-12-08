3d08c9dbe9d70dada76180a037fa1570e76cc27c
w.bitar@idscanbiometrics.com <w.bitar@idscan.co.uk>
2016-12-08 12:21:10 +0300

    first simple HttpModule

---
+++ src/WebApi/Helpers/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class Helpers
+    {
+        public static void Write(string stage, IPrincipal principal)
+        {
+            Debug.WriteLine($"-----{stage}-----");
+            if (principal == null || principal.Identity == null || !principal.Identity.IsAuthenticated)
+                Debug.WriteLine($"-Anonymous user-");
+            else
+                Debug.WriteLine($"-User: {principal.Identity.Name}-");
+
+            Debug.WriteLine("\n");
+        }
+    }
+}
---
+++ src/WebApi/Pipeline/HttpModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApi
+{
+    public class HttpModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += Context_BeginRequest;
+        }
+
+        private void Context_BeginRequest(object sender, EventArgs e)
+        {
+            Helpers.Write("HttpModule", HttpContext.Current.User);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
