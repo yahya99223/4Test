dce217cebc15bb8ec2e00b2ee5d822d72e6318e5
w.bitar <w.bitar@idscan.com>
2017-02-28 11:14:18 +0300

    Added shared library

---
+++ src/Shared/Shared.Messaging/Letter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Shared.Messaging
+{
+    public class Letter
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime SendingDate { get; set; }
+    }
+}
