6619c4b09e00735baba635e50e56488e83746d0e
w.bitar <w.bitar@idscan.com>
2017-05-16 13:59:18 +0300

    CosmosDb ready

---
+++ src/WebApp/Controllers/TasksController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class TasksController : Controller
+    {
+        private UserTaskStore store;
+
+        public TasksController()
+        {
+            store = new UserTaskStore();
+        }
+
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            var tasks = store.GetAll();
+            return View(tasks);
+        }
+
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(UserTask task)
+        {
+            task.id = Guid.NewGuid();
+            task.CreateDate = DateTime.UtcNow;
+            
+            await store.Add(task);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Details(Guid id)
+        {
+            var task = await store.GetById(id);
+            return View(task);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            await store.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/WebApp/Models/UserTask.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Azure.Documents.Client;
+
+namespace WebApp.Models
+{
+    public class UserTask
+    {
+        public UserTask()
+        {
+            Notes = new List<Note>();
+        }
+
+        public Guid id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int DonePercent { get; set; }
+        public DateTime CreateDate { get; set; }
+        public ICollection<Note> Notes { get; set; }
+    }
+
+    public class Note
+    {
+        public string Text { get; set; }
+        public DateTime AddingDate { get; set; }
+    }
+
+    public class UserTaskStore
+    {
+        private readonly DocumentClient client;
+        private readonly Uri tasksLink;
+        private const string databaseId = "wb-t-4test-mvcapp-cosmosdb";
+        private const string collectionId = "Tasks";
+
+        public UserTaskStore()
+        {
+            client = new DocumentClient(new Uri("https://wb-t-4test-mvcapp-cosmosdb.documents.azure.com:443/"), "nQwJRHrt6412W2uq8qtpd6ju9MaQ9F2uybSjBtK6wnZXNTuI7aR0aShDc6zUX2zieq6uxTM15oqYYSgutHEInA==");
+            tasksLink = UriFactory.CreateDocumentCollectionUri(databaseId, collectionId);
+        }
+
+        public IEnumerable<UserTask> GetAll()
+        {
+            var query = client.CreateDocumentQuery<UserTask>(tasksLink);
+            return query.OrderBy(x => x.CreateDate);
+        }
+
+        public async Task<UserTask> GetById(Guid id)
+        {
+            var documentLink = UriFactory.CreateDocumentUri(databaseId, collectionId, id.ToString());
+            return await client.ReadDocumentAsync<UserTask>(documentLink);
+        }
+
+        public async Task Add(UserTask task)
+        {
+            await client.CreateDocumentAsync(tasksLink, task);
+        }
+
+        public async Task Delete(Guid taskId)
+        {
+            var documentLink = UriFactory.CreateDocumentUri(databaseId, collectionId, taskId.ToString());
+            await client.DeleteDocumentAsync(documentLink);
+        }
+    }
+}
