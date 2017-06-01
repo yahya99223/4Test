1f89e4dec840e570caf4203b19096174323c86ff
w.bitar <w.bitar@idscan.com>
2017-06-01 10:34:51 +0300

    simple example

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using ConsoleApp.Actors;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static ActorSystem movieStreamingActorSystem;
+        static void Main(string[] args)
+        {
+            movieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+            Console.WriteLine("Actor System Created");
+
+            var playbackActorProps = Props.Create<PlaybackActor>();
+
+            var playbackActorRef = movieStreamingActorSystem.ActorOf(playbackActorProps, "PlaybackActor");
+
+            playbackActorRef.Tell("a bug's life");
+
+            Console.ReadLine();
+            movieStreamingActorSystem.Terminate().GetAwaiter().GetResult();
+        }
+
+
+    }
+}
