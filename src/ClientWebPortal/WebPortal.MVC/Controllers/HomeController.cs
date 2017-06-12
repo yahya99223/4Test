e0322ca42096f1702f63547dde77768331280924
w.bitar <w.bitar@idscan.com>
2017-06-12 18:27:01 +0300

    Added web app

---
+++ src/ClientWebPortal/WebPortal.MVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Micro.Shared.Messaging;
+
+namespace WebPortal.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ProcessTextCommand message)
+        {
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ src/ClientWebPortal/WebPortal.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebPortal.MVC.MvcApplication" Language="C#" %>
