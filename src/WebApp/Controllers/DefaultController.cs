fcbe9c030d1dbee7d741b295b2fd1568fd037d07
w.bitar <w.bitar@idscan.com>
2017-08-22 16:07:16 +0300

    Added Owin to the API

---
+++ src/WebApp/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class TestController : ApiController
+    {
+        [Route("api/test")]
+        public IHttpActionResult Get()
+        {
+            return Ok("API is OK");
+        }
+    }
+}
---
+++ src/WebApp/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using WebApp;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configure(config =>
+            {
+                WebApiConfig.Register(config);
+            });
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(GlobalConfiguration.Configuration);
+        }
+    }
+}
