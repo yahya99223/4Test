9d20724e0a9d32ce50a4270b46b1740933bc80d3
w.bitar <w.bitar@idscan.com>
2017-08-23 08:24:28 +0300

    added unity package

---
+++ src/IoC.Resolver/ServiceResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDScan.SaaS.SharedBlocks.Helpers.Core;
+using Microsoft.Practices.Unity;
+
+namespace IoC.Resolver
+{
+    public class ServiceResolver : IServiceResolver
+    {
+        public ServiceResolver()
+        {
+            var container = new UnityContainer();
+            container.RegisterInstance<IServiceResolver>(this,new ContainerControlledLifetimeManager());
+
+        }
+        public T Resolve<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> ResolveAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
