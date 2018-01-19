dd98e8ceacb6fb0100d9d5dcf9b774ae2dbde62a
Wahid Bitar <w.bitar@idscan.com>
2018-01-19 14:08:12 +0300

    Added the initial services projects

---
+++ src/Message.Contracts/MessagingConstants.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Message.Contracts
+{
+    public static class MessagingConstants
+    {
+        public const string MqUri = "rabbitmq://localhost/IDScanSaaSWahid/";
+        public const string UserName = "wahid";
+        public const string Password = "W@123123";
+
+
+        public const string OrderManagementQueue = "OrderManagement";
+        public const string CapitalizeServiceQueue = "Capitalize.Service";
+        public const string NormalizeServiceQueue = "Normalize.Service";
+        public const string ValidateServiceQueue = "Validate.Service";
+        public const string SagaQueue = "Saga.Service";
+    }
+}
